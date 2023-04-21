b032333c9c28a26d35a9bf9fed9e1740765126f1
Ryan Kerry <ryan2kerry@gmail.com>
2023-04-21 05:52:03 +0100

    Refactored & Fixed Movement alignment
    Changed the way the script calculates the relative transformation of the object.
    It now creates a vector of the relative forward and right of the object, and multiplies them by the respective raw input axis.

--- Assets/Abstract/PlayerInputManager.cs
+++ Assets/Abstract/PlayerInputManager.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PlayerInputManager : MonoBehaviour, PlayerControls.IDefaultPlayerActions
 {
-    public GameObject playerCamera;
+    public GameObject cameraObject;
     public CharacterController playerCharacterController;
     public PlayerControls playerControls;
-    public Vector3 frameInput;
-    public Vector3 cameraForward;
-    public float movementSpeed;
-
+    
+    public Vector3 RawInputDirection;
+    //public Vector3 RotatedInputDirection;
+    
+    [SerializeField] private Vector3 cameraForward;
+    
+    public float movementSpeed = 5f;
     public float mouseSpeed = 100f;
 
     private void Awake()
@@ -21,27 +25,34 @@ public class PlayerInputManager : MonoBehaviour, PlayerControls.IDefaultPlayerAc
         playerControls.DefaultPlayer.SetCallbacks(this);
 
         playerCharacterController = GetComponent<CharacterController>();
-        playerCamera = GetComponentInChildren<CinemachineVirtualCamera>().gameObject;
+        cameraObject = GetComponentInChildren<CinemachineVirtualCamera>().gameObject;
     }
 
     private void Update()
     {
-        cameraForward = playerCamera.transform.TransformDirection(Vector3.forward);
-        playerCharacterController.Move(frameInput * movementSpeed * Time.deltaTime);
+        //Apply movement to the player
+
+
+        //cameraForward = cameraObject.transform.TransformDirection(Vector3.forward);
+        //RotatedInputDirection = Quaternion.LookRotation(cameraForward, Vector3.up) * RawInputDirection;
+        /*Vector3 fwd = transform.forward * RawInputDirection.z;
+        Vector3 rgt = transform.right * RawInputDirection.x;*/
+        
+        
+        Vector3 relativeMovement = transform.forward * RawInputDirection.z + transform.right * RawInputDirection.x;
+        playerCharacterController.Move( movementSpeed * Time.deltaTime * relativeMovement);
         if (!playerCharacterController.isGrounded) playerCharacterController.Move(Physics.gravity);
 
     }
 
     public void OnWalk(InputAction.CallbackContext context)
     {
-        frameInput = Vector3.zero;
+        RawInputDirection = Vector3.zero;
         if (context.performed)
         {
             Vector2 input = context.ReadValue<Vector2>();
-            Vector3 planeNormal = Vector3.up;
-            //Vector3 planePoint = transform.position;
-            Vector3 projectedInput = Vector3.ProjectOnPlane(new Vector3(input.x, 0, input.y), planeNormal);
-            frameInput = Quaternion.LookRotation(cameraForward, planeNormal) * projectedInput;
+            RawInputDirection = new Vector3(input.x, 0, input.y);
+            //Vector3 projectedInput = Vector3.ProjectOnPlane(new Vector3(input.x, 0, input.y), Vector3.up);
         }
     }
 
@@ -52,26 +63,14 @@ public class PlayerInputManager : MonoBehaviour, PlayerControls.IDefaultPlayerAc
             Vector2 mouseDelta = context.ReadValue<Vector2>();
             float mouseY = mouseDelta.y;
             float mouseX = mouseDelta.x;
-
-            // Rotate the player object left and right based on the mouse movement
+            
             gameObject.transform.Rotate(transform.up, mouseX * Time.deltaTime * mouseSpeed);
 
-            Vector3 cameraRotation = playerCamera.transform.localEulerAngles;
+            //Vector3 cameraRotation = cameraObject.transform.localEulerAngles;
             
             //cameraRotation.x = Mathf.Clamp(cameraRotation.x - mouseY * Time.deltaTime * mouseSpeed, -89f, 89);
-            //cameraRotation.x = cameraRotation.x - mouseY * Time.deltaTime * mouseSpeed;
-            //playerCamera.transform.localRotation = Quaternion.Euler(cameraRotation);
-            playerCamera.transform.Rotate(playerCamera.transform.right, -mouseY*Time.deltaTime*mouseSpeed, Space.World);
-            
-            
-            // Calculate a new movement vector based on the player's orientation
-            Vector3 forward = playerCamera.transform.forward;
-            forward.y = 0;
-            forward.Normalize();
-            Vector3 right = playerCamera.transform.right;
-            right.y = 0;
-            right.Normalize();
-            frameInput = forward * playerControls.DefaultPlayer.Walk.ReadValue<Vector2>().y + right * playerControls.DefaultPlayer.Walk.ReadValue<Vector2>().x;
+
+            cameraObject.transform.Rotate(cameraObject.transform.right, -mouseY*Time.deltaTime*mouseSpeed, Space.World);
         }
     }
 
