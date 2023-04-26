578f23224913c5fcae0a410ae1d9ecb852c6451a
Lucas Redmond <lucasr705@gmail.com>
2023-04-26 22:51:51 +0100

    Amendment  "Revert "Eye anim""
    This reverts commit b659f6a237b466f3073d654233a9c3550f9ba43a.

---
+++ Assets/Abstract/UIController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    public Sprite fourLives;
+    public Sprite threeLives;
+    public Sprite twoLives;
+    public Sprite oneLife;
+    public Image lives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        changeLives(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeLives(int x)
+    {
+        switch(x)
+        {
+            case 1:
+                lives.sprite = oneLife;
+                break;
+            case 2:
+                lives.sprite = twoLives;
+                break;
+            case 3:
+                lives.sprite = threeLives;
+                break;
+            case 4:
+                lives.sprite = fourLives;
+                break;
+            default:
+                lives.sprite = fourLives;
+                break;
+        }
+    }
+
+}
