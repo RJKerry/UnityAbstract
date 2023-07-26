b26acc81827d661a509719455947fee349b2e17a
Lucas Redmond <lucasr705@gmail.com>
2023-07-26 18:35:38 +0100

    HUD Design Implementation

--- Assets/Abstract/Player/ItemSystem/Portal/PortalItem.cs
+++ Assets/Abstract/Player/ItemSystem/Portal/PortalItem.cs
@@ -1,75 +1,76 @@
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
 
-[CreateAssetMenu(menuName = "ScriptableObjects/ItemBase/PortalItem")]
-public class PortalItem : ItemData
-{
-    //private Camera PortalCam;
+//[CreateAssetMenu(menuName = "ScriptableObjects/ItemBase/PortalItem")]
+//public class PortalItem : ItemData
+//{
+//    //private Camera PortalCam;
 
-    private Camera PlayerCam;
+//    private Camera PlayerCam;
 
-    //public RenderTexture portalTex;
-    //public GameObject portal;
+//    //public RenderTexture portalTex;
+//    //public GameObject portal;
 
-    //private bool portalActive = false;
+//    //private bool portalActive = false;
 
-    //private float tick;
+//    //private float tick;
 
-    // Update is called once per frame
+//    // Update is called once per frame
 
-    private Portal CurrentPortal;
+//  //  private Portal CurrentPortal;
 
-/*    private void Awake()
-    {
-        //PortalCam = portal.GetComponent<Camera>();
-    }*/
+///*    private void Awake()
+//    {
+//        //PortalCam = portal.GetComponent<Camera>();
+//    }*/
 
-/*    private void update()
-    {
-        if (portalActive)
-        {
-            // Calculate difference between player camera's rotation and portal's rotation
-            Quaternion camRotDiff = Quaternion.Inverse(PortalCam.transform.rotation) * PlayerCam.transform.rotation;
+///*    private void update()
+//    {
+//        if (portalActive)
+//        {
+//            // Calculate difference between player camera's rotation and portal's rotation
+//            Quaternion camRotDiff = Quaternion.Inverse(PortalCam.transform.rotation) * PlayerCam.transform.rotation;
 
-            // Apply difference to portal camera's rotation
-            PortalCam.transform.rotation = PortalCam.transform.rotation * camRotDiff;
+//            // Apply difference to portal camera's rotation
+//            PortalCam.transform.rotation = PortalCam.transform.rotation * camRotDiff;
 
-            tick += Time.deltaTime;
+//            tick += Time.deltaTime;
 
 
-            if (tick >= 10)
-            {
-                portalActive = false;
-                portal.transform.position = new Vector3(0, 0, 0);
-            }
-        }
-    }*/
+//            if (tick >= 10)
+//            {
+//                portalActive = false;
+//                portal.transform.position = new Vector3(0, 0, 0);
+//            }
+//        }
+//    }*/
 
-    public override void ItemUsed()
-    {
-        Debug.Log("Portal item has been used");
+//    public override void ItemUsed()
+//    {
+//        Debug.Log("Portal item has been used");
 
-        RaycastHit hit;
-        if (Physics.Raycast(PlayerCam.transform.position, PlayerCam.transform.forward, out hit))
-        {
-            if (hit.transform.CompareTag("Wall"))
-            {
-                Instantiate(portal, hit.point, Quaternion.LookRotation(hit.normal, Vector3.up) * Quaternion.Euler(90, 0, 0));
+//        RaycastHit hit;
 
-                // Calculate position of camera on the other side of the wall
-                Vector3 cameraPos = hit.point + hit.normal * 0.01f;
+//        if (Physics.Raycast(PlayerCam.transform.position, PlayerCam.transform.forward, out hit))
+//        {
+//            if (hit.transform.CompareTag("Wall"))
+//            {
+//                Instantiate(portal, hit.point, Quaternion.LookRotation(hit.normal, Vector3.up) * Quaternion.Euler(90, 0, 0));
 
-                // Set camera's render texture to the portal texture
-                PortalCam.targetTexture = portalTex;
+//                // Calculate position of camera on the other side of the wall
+//                Vector3 cameraPos = hit.point + hit.normal * 0.01f;
 
-                // Set camera's position
-                PortalCam.transform.position = cameraPos;
-                portalActive = true;
-                //tick = Time.deltaTime;
-            }
-        }
-    }
-}
+//                // Set camera's render texture to the portal texture
+//                PortalCam.targetTexture = portalTex;
+
+//                // Set camera's position
+//                PortalCam.transform.position = cameraPos;
+//                portalActive = true;
+//                //tick = Time.deltaTime;
+//            }
+//        }
+//    }
+//}
