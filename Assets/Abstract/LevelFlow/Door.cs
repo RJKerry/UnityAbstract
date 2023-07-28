b691ca89dd7cbf4eb8053e32e5abbb5a9363f45f
Ryan Kerry <ryan2kerry@gmail.com>
2023-07-28 23:08:17 +0100

    FMOD Door integration
    Refactored door motion and sequencing logic to use Coroutines
    Implemented FMOD door sound effects

--- Assets/Abstract/LevelFlow/Door.cs
+++ Assets/Abstract/LevelFlow/Door.cs
@@ -1,65 +1,81 @@
+using FMOD;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Door : MonoBehaviour, ITerminalListener, IInteractable
 {
-    public Vector3 StartPos, EndPos;
+    public Vector3 ClosePos, OpenPos;
+
     public bool triggered;
+    public bool Unlocked = true;
+    [SerializeField]    private bool transitioning = false;
 
-    public bool Unlocked;
 
-    float eTime, tTime = 1; //elapsed time, target time
+    public float eTime, tTime = 1f; //elapsed time, target time
     public Light LockedLight, UnlockedLight;
 
     public int _IDGroup; //Nescesarry for anything implementing ITerminalListener - Trigger ID Group
     public int IDGroup { get => _IDGroup; set => _IDGroup = value; } //Better syntax
 
+    public float UnlockDelay = 5f;
+
+    public string 
+        DoorOpen = "event:/Door/DoorOpen",
+        DoorClose = "event:/Door/DoorClose", 
+        DoorUnlock = "event:/Door/DoorUnlock";
+
     private void Awake()
     {
-        StartPos = transform.position;
-        EndPos = transform.position + Vector3.up * 5;
+        ClosePos = transform.position;
+        OpenPos = transform.position+Vector3.up*3;
     }
 
-    /// <summary>
-    /// Update driven lerp is not a good thing, but this is for testing 
-    /// </summary>
-    void Update()
+    public IEnumerator OpenDoor(Vector3 StartPos, Vector3 EndPos)
     {
+        if (transitioning)
+            yield break; //Cannot Occur if already in motion
 
-    }
+        transitioning = true;
 
-    public IEnumerator Activated(Vector3 startPos, Vector3 endPos)
-    {
-        if (true)
+        for (int i = 0; i <= 1; i++)
         {
+            eTime = 0;
+            FMODUnity.RuntimeManager.PlayOneShot(i == 0 ? DoorOpen : DoorClose, transform.position);
+            yield return new WaitForSecondsRealtime(0.4f); //works to offset audio issue, this will be fixed
+
             while (eTime < tTime)
             {
-                transform.position = Vector3.Lerp(StartPos, EndPos, eTime / tTime);
+                transform.position = Vector3.Lerp(i == 0 ? StartPos : EndPos, i == 0 ? EndPos : StartPos, eTime / tTime);
                 eTime += Time.deltaTime;
                 yield return null;
             }
+            yield return new WaitForSecondsRealtime(2f);
         }
-    }
-
-    public void OnActivated()
-    {
-        if (!Unlocked)
-        {
-            Debug.Log("I am a door");
-            //Unlocked = true;
-        } 
+        transitioning = false;
     }
 
     public void OnInteract(PlayerInputManager messageSource)
     {
-        if (Unlocked) UnlockSequence();
+        if (Unlocked) StartCoroutine(OpenDoor(ClosePos, OpenPos));
     }
 
-    public void UnlockSequence()
+    public void OnActivated()
+    {
+        //Unlock
+        if (!Unlocked)
+        {
+            StartCoroutine(UnlockSequence());
+        }
+    }
+
+    public IEnumerator UnlockSequence()
     {
         LockedLight.gameObject.SetActive(false);
+        yield return new WaitForSecondsRealtime(UnlockDelay);
         UnlockedLight.gameObject.SetActive(true);
-
+        Unlocked = true;
+        FMODUnity.RuntimeManager.PlayOneShot(DoorUnlock, transform.position);
     }
 }
