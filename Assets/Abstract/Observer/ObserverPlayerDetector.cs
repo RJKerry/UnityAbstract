6f3e78cd679e23726f07628d6afc4a43ad5a686d
Ryan Kerry <ryan2kerry@gmail.com>
2023-08-03 22:44:39 +0100

    Observer now cast to player
    Cast for player now calculated with a debug, refactored structure & debugs for testing
    Playermanager.PlayerPosition now has bool parameter "AddHalfHeightToUP" - adds halfheight of the playercapsule to the returned vector position value

--- Assets/Abstract/Observer/ObserverPlayerDetector.cs
+++ Assets/Abstract/Observer/ObserverPlayerDetector.cs
@@ -9,6 +9,8 @@ public class ObserverPlayerDetector : MonoBehaviour
 
     public PlayerManager Player;
 
+    private Transform RayOrigin;
+
     private void OnTriggerEnter(Collider other)
     {
         PlayerManager manager = other.gameObject.GetComponent<PlayerManager>();
@@ -18,14 +20,30 @@ public class ObserverPlayerDetector : MonoBehaviour
         }
     }
 
+    private void Awake()
+    {
+        RayOrigin = transform.parent;
+    }
+
     private void Update() //This will need changing eventually to not rapidfire
     {
-        if (Player != null) 
+        if (Player != null)
         {
+            Debug.Log("Not Null");
             RaycastHit HitObject;
-            if (Physics.Raycast(transform.position, transform.position-Player.PlayerPosition().normalized, out HitObject))
-                if(HitObject.transform.GetComponent<PlayerManager>() == Player)
+
+            Debug.DrawRay(RayOrigin.position, Player.PlayerPosition(true) - RayOrigin.position, Color.cyan);
+            if (Physics.Raycast(RayOrigin.position, Player.PlayerPosition(true) - RayOrigin.position, out HitObject))
+            {
+                if (HitObject.transform.GetComponent<PlayerManager>() == Player)
+                {
                     Player.OnDamageRecieved(Damage * DamageScalar);
+                }
+                else
+                    Debug.Log("Hit object is not player");
+            }
+            else
+                Debug.Log("Cast missed");
         }
     }
 
@@ -34,5 +52,4 @@ public class ObserverPlayerDetector : MonoBehaviour
         if(other.gameObject == Player.gameObject)
             Player = null;
     }
-
 }
--- Assets/Abstract/Player/PlayerManager.cs
+++ Assets/Abstract/Player/PlayerManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerManager : HealthHandler
 {
-    public Vector3 PlayerPosition()
-    { return transform.position; }
+    CapsuleCollider playerCapsule;
+
+    private void Awake()
+    {
+        playerCapsule = GetComponent<CapsuleCollider>();
+    }
+
+    public Vector3 PlayerPosition(bool AddHalfHeightToUp)
+    { return AddHalfHeightToUp ? transform.position : transform.position + Vector3.up * playerCapsule.height / 2; }
 }
